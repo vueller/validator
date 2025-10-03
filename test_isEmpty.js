f9625707f81fa0bc6e665209ef4b48333e457218
Gustavo Will da Silva <48598429+gitgusilva@users.noreply.github.com>
2025-10-03 11:59:08 -0300

    Delete test_isEmpty.js

--- test_isEmpty.js
+++
@@ -1,6 +0,0 @@
-import { isEmpty } from './src/utils/index.js';
-
-console.log('isEmpty("123"):', isEmpty('123'));
-console.log('isEmpty(""):', isEmpty(''));
-console.log('isEmpty(null):', isEmpty(null));
-console.log('isEmpty(undefined):', isEmpty(undefined));
